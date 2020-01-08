050826ced89d256fd5f48e1b634d75fcd33b6edb
Timofey Martynov <feymartynov@gmail.com>
2020-01-08 06:56:49 +0300

    Add the concept from readme to docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
+//! This is an experimental high-level Rust binding to
+//! [Janus Gateway](https://github.com/meetecho/janus-gateway)'s application plugin API.
+//! 
+//! **WARNING!** This project is an experimental and not tested WIP.
+//! Don't try to use it for creating actual plugins.
+//! 
+//! # The concept
+//! 
+//! There's already [janus-plugin](https://github.com/mozilla/janus-plugin-rs) crate which enables
+//! creating plugins for Janus Gateway but its API is
+//! [too low-level and unsafe](https://github.com/mozilla/janus-plugin-rs/issues/10).
+//! 
+//! This crate enables writing plugins in a more idiomatic Rust way:
+//! 
+//! * Plugin code has nothing to do with raw pointers and unsafe C functions. These things are abstracted out by the crate.
+//! * A plugin is a trait implementation, not a bunch of `extern "C"` functions.
+//! * Object-oriented API instead of procedural.
+//! * A plugin and each of its handles may have their state.
+//! * Plugin handles' core C state is not mixed together with plugin's Rust state.
+//! * Dispatching callbacks from other plugin's threads is thread safe. This enables the plugin to handle messages and media events in an asynchronous non-blocking way.
+//! * [Serde](https://github.com/serde-rs/serde) library is being used for (de)serialization within the plugin as de-facto Rust's standard. No need to tackle C's [Jansson](https://github.com/akheron/jansson) that is being used on the low-level API.
+//! * Unit testing is possible for plugins because they aren't coupled to C code.
+//!
+//!
 //! # How do I make a plugin?
 //!
 //! The simpliest way is to copy-paste and change the
